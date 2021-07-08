e62313b4b020f207ab25b9fef2475cb5f2bb785e
Giovani Fernandes <giovani_oli@hotmail.com>
2021-07-08 15:23:27 -0400

    wip: enrollment api

---
+++ spec/requests/enrollments_api_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'Enrollment from courses API' do
+  context 'GET /api/v1/courses/:id/enrollments' do
+    it 'should get enrollments from course' do
+      instructor = Instructor.create!(name: 'Fulano Sicrano',
+                                      email: 'fulano@codeplay.com.br')
+      course = Course.create!(name: 'Ruby', description: 'Um curso de Ruby',
+                     code: 'RUBYBASIC', price: 10,
+                     enrollment_deadline: '22/12/2033', instructor: instructor)
+      student = Student.create!(email: 'student@email.com', password: 123456)
+      other_student = Student.create!(email: 'johndoe@email.com', password: 123456)
+      Enrollment.create!(course: course, price: course.price, student: student)
+      Enrollment.create!(course: course, price: course.price, student: other_student)
+
+      get '/api/v1/courses/:id/enrollments', params: { id: course.id }
+
+      expect(response).to have_http_status(200)
+      expect(response.content_type).to include('application/json')
+      expect(parsed_body.count).to eq(2)
+      expect(parsed_body['course']).to eq('Ruby')
+      expect(parsed_body[0]['email']).to eq('student@email.com')
+      expect(parsed_body[1]['email']).to eq('johndoe@email.com')
+    end
+  end
+
+  private
+
+  def parsed_body
+    JSON.parse(response.body)
+  end
+end
